44db61d16fec8e197ec9db992eb70a5f5cb6bb09
Jeffry Ghazally <jeff@bigfish.co.uk>
2013-08-18 15:13:39 +1200

    Add in form me ribbon

--- quakemonitor.php
+++ quakemonitor.php
@@ -44,7 +44,7 @@ src="https://maps.googleapis.com/maps/api/js?key=<?php echo GOOGLEMAPS_API ?>Q&s
 		<link href="css/screen.css" rel="stylesheet">
 	</head>
 	<body>
-
+<a href="https://github.com/jghazally/quakemonitor"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_green_007200.png" alt="Fork me on GitHub"></a>
 		<div class="container">
 		<div class="page-header">
 			<h1>Quake monitor</h1>
